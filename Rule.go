4151d80ea30d70b048e3d1de23f1ec13dae5322b
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2023-08-26 01:24:08 +0200

    Add MapRule

--- Rule.go
+++ Rule.go
@@ -12,3 +12,53 @@ func RunRule[ReadT any, OutT any, ExpectT any](
 	go rule(reader, resultChannel)
 	return <-resultChannel
 }
+
+func MapRule[ReadT any, FromT any, ToT any, ExpectT any](
+	formatPacket func(*Packet[ReadT]) string,
+	unmapped ExpectT,
+	formatUnmapped func(ExpectT) string,
+	innerRule Rule[ReadT, FromT, ExpectT],
+	mapping func(FromT) ToT,
+) Rule[ReadT, ToT, ExpectT] {
+	return func(reader *Reader[ReadT], resultChannel ResultChannel[ReadT, ToT, ExpectT]) {
+		if debugOn {
+			debugf("Entering MapRule with Reader %s\n", debugReader(reader))
+		}
+		var outValue ToT
+		var result *Result[ReadT, ToT, ExpectT]
+		if innerRule == nil {
+			if debugOn {
+				debugf("[MapRule with Reader %s] Missing inner rule\n", debugReader(reader))
+			}
+			if formatUnmapped == nil {
+				formatUnmapped = func(ExpectT) string {
+					return "<missing mapped rule not formattable>"
+				}
+			}
+			current := reader.Current()
+			result = &Result[ReadT, ToT, ExpectT] {
+				Offset: current.Offset,
+				Result: outValue,
+				Error: &SyntaxError[ReadT, ExpectT] {
+					Found: current,
+					Expected: []ExpectT {unmapped},
+					FormatFound: formatPacket,
+					FormatExpected: formatUnmapped,
+				},
+				Reader: reader,
+			}
+		} else {
+			if debugOn {
+				debugf("[MapRule with Reader %s] Deletating to inner rule\n", debugReader(reader))
+			}
+			result = MapResult(RunRule(innerRule, reader), mapping)
+		}
+		if debugOn {
+			debugf("[MapRule with Reader %s] Issuing %s\n", debugReader(reader), debugResult(result))
+		}
+		resultChannel <- result
+		if debugOn {
+			debugf("Leaving MapRule with Reader %s\n", debugReader(reader))
+		}
+	}
+}
